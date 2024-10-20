a30b3a99d96ad858443b350f682d052da76d7d5a
mnajib-uw <binmohdmohta@wisc.edu>
2024-10-20 14:36:52 -0500

    Add skeleton navigation

--- app/src/main/java/com/cs407/studentbazaar/LoginActivity.kt
+++ app/src/main/java/com/cs407/studentbazaar/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cs407.lab5_milestone.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
